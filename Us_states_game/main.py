e86a77f6c2eb062da8293e3c775e5bf557972762
Aleks <kan.python@gmail.com>
2021-12-06 14:54:00 +0300

    Added: functionality to clear the data from states_to_learn.csv and writing new data on Exit the game

--- Us_states_game/main.py
+++ Us_states_game/main.py
@@ -1,6 +1,7 @@
 import turtle
 import pandas as pd
 
+# Setting field for the game
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
@@ -17,6 +18,7 @@ game_is_on = True
 while game_is_on:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state name?").title()
+    # Checking if the answer is correct
     if answer_state in states_list:
         states_correct += 1
         t = turtle.Turtle()
@@ -27,10 +29,14 @@ while game_is_on:
         t.write(f"{answer_state}")
         guessed_states.append(answer_state)
 
+    # Exiting the game and creating a file with states left to learn
     if answer_state == 'Exit':
         states_to_learn = pd.DataFrame([x for x in states_list if x not in guessed_states])
-        states_to_learn.to_csv("states_to_learn.csv")
+        # Clearing data from file and writing new data
+        with open('states_to_learn.csv', 'w') as file:
+            file.truncate()
+            file.write(states_to_learn.to_csv())
+
         break
 
 screen.exitonclick()
-
